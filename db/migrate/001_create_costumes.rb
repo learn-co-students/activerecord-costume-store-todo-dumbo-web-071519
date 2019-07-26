da0447ebca6f84c562a5262ec00984ca395ff49d
Martin Crabtree <martin.crabtree@gmail.com>
2019-07-25 23:19:00 -0400

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -7,3 +7,19 @@
 # Define a change method in which to do the migration
 # In this change method, create columns with the correct names and 
 # value types according to the spec
+
+class CreateCostumes < ActiveRecord::Migration[4.2]
+
+    def change
+        create_table :costumes do |table_helper|
+            table_helper.string :name
+            table_helper.float :price
+            table_helper.string :size
+            table_helper.string :image_url
+
+            table_helper.timestamps null: false
+
+        end
+    end
+
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,19 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+
+    def change
+        create_table :haunted_houses do |table_helper|
+            table_helper.string :name
+            table_helper.string :location
+            table_helper.float :price
+            table_helper.string :theme
+            table_helper.boolean :family_friendly
+            table_helper.datetime :opening_date
+            table_helper.datetime :closing_date
+            table_helper.string :description
+
+            table_helper.timestamps null: false
+        end
+    end
+
+end
